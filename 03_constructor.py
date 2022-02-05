24238d496a4bbd3882a404ac8b8b1890bb9524a6
Robert Vári <mail.robertvari@gmail.com>
2022-02-05 14:50:52 +0100

    update

--- 03_constructor.py
+++ 03_constructor.py
@@ -1,14 +1,16 @@
 class Dice:
     # constructor of a class
     def __init__(self, sides, color):
+        # instance attribute
         self.sides = sides
         self.color = color
+
+        # variable
+        name = "robert"
 
 
 my_dice1 = Dice(6, "white")
 my_dice2 = Dice(10, "blue")
 my_dice3 = Dice(20, "red")
 
-print(my_dice1.sides, my_dice1.color)
-print(my_dice2.sides, my_dice2.color)
-print(my_dice3.sides, my_dice3.color)
+my_dice1.color
