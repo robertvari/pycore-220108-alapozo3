c5e89402912d68c869c0f47c07f12fe7f03caad8
Robert Vári <mail.robertvari@gmail.com>
2022-02-05 16:35:31 +0100

    update

--- 08_special_methods.py
+++ 08_special_methods.py
@@ -24,9 +24,20 @@ class Person:
     def __str__(self):
         return f"Name: {self._name}\nAge: {self._age}\nEmail: {self._email}"
 
+    def __repr__(self):
+        return self._name
+
 
 my_dice1 = Dice("White", 6)
 my_dice2 = Dice("Red", 10)
+my_dice3 = Dice("Blue", 20)
+my_dices = [my_dice1,  my_dice2, my_dice3]
 
-my_person = Person("Robert", 34, "robert@gmail.com")
-print(my_person)
+my_person1 = Person("Robert", 34, "robert@gmail.com")
+my_person2 = Person("Csaba", 20, "csaba@gmail.com")
+my_person3 = Person("Kriszta", 26, "kriszta@gmail.com")
+my_persons = [my_person1, my_person2, my_person3]
+
+print(my_dices)
+print(my_persons)
+print(my_person3)
