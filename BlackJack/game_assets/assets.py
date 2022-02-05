61064db3a41b9b299a132a6acee97ccdb4622965
Robert Vári <mail.robertvari@gmail.com>
2022-02-05 17:17:45 +0100

    update

--- BlackJack/game_assets/assets.py
+++ BlackJack/game_assets/assets.py
@@ -1,5 +1,10 @@
 class Card:
-    pass
+    def __init__(self, name, value):
+        self._name = name
+        self._value = value
+
+    def __repr__(self):
+        return f"{self._name} {self._value}"
 
 
 class Deck:
@@ -7,4 +12,15 @@ class Deck:
 
 
 class Player:
-    pass
+    def __init__(self):
+        self._hande = []
+
+
+# testing my assets
+if __name__ == '__main__':
+    card1 = Card("Club Ace", 11)
+    card2 = Card("Club 3", 3)
+    card3 = Card("Spade 7", 7)
+
+    hande = [card1, card2, card3]
+    print(hande)
