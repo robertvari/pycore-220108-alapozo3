f9735d3f1b9447187ef54f8f1945756e809282ac
Robert Vári <mail.robertvari@gmail.com>
2022-02-05 17:03:23 +0100

    project init

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -1,6 +1,14 @@
 class BlackJack:
     def __init__(self):
-        pass
+        self._intro()
 
+        # todo create a deck of card
+        # todo create a player
+        # todo AI player
+        # todo bet
+
+    # protected method
+    def _intro(self):
+        print("="*50, "BlackJack Game", "="*50,)
 
 BlackJack()
