24219ad46b0ad9b73be51f344cafd57f7cf16220
Robert Vári <mail.robertvari@gmail.com>
2022-02-05 14:44:20 +0100

    update

--- 03_constructor.py
+++ 03_constructor.py
@@ -1,7 +1,14 @@
 class Dice:
     # constructor of a class
-    def __init__(self):
-        print("A dice is born!!!!")
+    def __init__(self, sides, color):
+        self.sides = sides
+        self.color = color
 
 
-my_dice1 = Dice()
+my_dice1 = Dice(6, "white")
+my_dice2 = Dice(10, "blue")
+my_dice3 = Dice(20, "red")
+
+print(my_dice1.sides, my_dice1.color)
+print(my_dice2.sides, my_dice2.color)
+print(my_dice3.sides, my_dice3.color)
