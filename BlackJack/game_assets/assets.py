71a2ad8d30986432cc8c072ddbe9b3abcde140b5
Robert Vári <mail.robertvari@gmail.com>
2022-02-05 17:29:05 +0100

    update

--- BlackJack/game_assets/assets.py
+++ BlackJack/game_assets/assets.py
@@ -11,9 +11,33 @@ class Deck:
     def __init__(self):
         self._cards = []
 
+        self.create()
+
     @property
     def cards(self):
         return self._cards
+
+    def create(self):
+        self._cards.clear()
+
+        cards = [
+            ["2", 2],
+            ["3", 3],
+            ["4", 4],
+            ["5", 5],
+            ["6", 6],
+            ["7", 7],
+            ["8", 8],
+            ["9", 9],
+            ["10", 10],
+            ["King", 10],
+            ["Queen", 10],
+            ["Jack", 10],
+            ["Ace", 11]
+        ]
+
+        names = ["Heart", "Club", "Diamond", "Spade"]
+
 
 class Player:
     def __init__(self):
