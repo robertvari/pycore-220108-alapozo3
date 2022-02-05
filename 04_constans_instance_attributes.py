491de2f34ffaa4aab81a32f9d054cae1b6790222
Robert Vári <mail.robertvari@gmail.com>
2022-02-05 15:15:14 +0100

    update

---
+++ 04_constans_instance_attributes.py
@@ -0,0 +1,17 @@
+class Dice:
+    # class constant attributes
+    dices_created = 0
+
+    def __init__(self, color, sides):
+        self.color = color
+        self.sides = sides
+        Dice.dices_created += 1
+
+
+my_dice1 = Dice("white", 6)
+my_dice2 = Dice("white", 6)
+my_dice3 = Dice("white", 6)
+my_dice4 = Dice("white", 6)
+
+print(my_dice1.dices_created)
+print(my_dice4.dices_created)
