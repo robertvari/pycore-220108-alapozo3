2af04139f01835e840b0899f84dcd7b11ab67fb0
Robert Vári <mail.robertvari@gmail.com>
2022-02-06 16:58:26 +0100

    project init

--- BattleOfClasses/game_assets/characters.py
+++ BattleOfClasses/game_assets/characters.py
@@ -1,3 +1,6 @@
+import random
+
+
 class CharacterBase:
     def __init__(self):
         # base attributes
@@ -15,3 +18,33 @@ class CharacterBase:
         self._strength = 0
         self._max_HP = 100
         self._current_HP = 0
+
+    @staticmethod
+    def get_fantasy_name():
+        FIRST = ['A', 'Ag', 'Ar', 'Ara', 'Anu', 'Bal', 'Bil', 'Boro', 'Bern', 'Bra', 'Cas', 'Cere', 'Co', 'Con',
+                 'Cor', 'Dag', 'Doo', 'Elen', 'El', 'En', 'Eo', 'Faf', 'Fan', 'Fara', 'Fre', 'Fro', 'Ga', 'Gala', 'Has',
+                 'He', 'Heim', 'Ho', 'Isil', 'In', 'Ini', 'Is', 'Ka', 'Kuo', 'Lance', 'Lo', 'Ma', 'Mag', 'Mi', 'Mo',
+                 'Moon', 'Mor', 'Mora', 'Nin', 'O', 'Obi', 'Og', 'Pelli', 'Por', 'Ran', 'Rud', 'Sam', 'She', 'Sheel',
+                 'Shin', 'Shog', 'Son', 'Sur', 'Theo', 'Tho', 'Tris', 'U', 'Uh', 'Ul', 'Vap', 'Vish', 'Ya', 'Yo', 'Yyr']
+
+        SECOND = ['ba', 'bis', 'bo', 'bus', 'da', 'dal', 'dagz', 'den', 'di', 'dil', 'din', 'do', 'dor', 'dra',
+                  'dur', 'gi', 'gauble', 'gen', 'glum', 'go', 'gorn', 'goth', 'had', 'hard', 'is', 'ki', 'koon', 'ku',
+                  'lad', 'ler', 'li', 'lot', 'ma', 'man', 'mir', 'mus', 'nan', 'ni', 'nor', 'nu', 'pian', 'ra', 'rak',
+                  'ric', 'rin', 'rum', 'rus', 'rut', 'sek', 'sha', 'thos', 'thur', 'toa', 'tu', 'tur', 'tred', 'varl',
+                  'wain', 'wan', 'win', 'wise', 'ya']
+
+        return f"{random.choice(FIRST)}{random.choice(SECOND)}"
+
+
+class Player(CharacterBase):
+    pass
+
+
+class Enemy(CharacterBase):
+    pass
+
+
+class NPC(Enemy):
+    pass
+
+
