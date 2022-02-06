82f9d26f7ac6ce8c725f2a9630b7e5bcbf102d31
Robert Vári <mail.robertvari@gmail.com>
2022-02-06 18:08:18 +0100

    project init

--- BattleOfClasses/game_assets/places.py
+++ BattleOfClasses/game_assets/places.py
@@ -1,16 +1,22 @@
 class PlaceBase:
-    def __init__(self):
+    def __init__(self, main_game):
         self._name = None
+        self._main_game = main_game
 
-    def enter(self, character):
+    def enter(self):
         pass
 
 
 class Tavern(PlaceBase):
-    def enter(self, player):
-        print(f"Welcome in the tavern {player}. I you have gold, you are in the right place.")
+    def enter(self):
+        self._main_game.clear_screen()
+        print(f"Welcome in the tavern {self._main_game.player}. I you have gold, you are in the right place.")
 
 
 class Arena(PlaceBase):
-    def enter(self, player):
-        print(f"You are in the arena {player}. Fight for your life!")
+    def enter(self):
+        self._main_game.clear_screen()
+        print(f"You are in the arena {self._main_game.player}. Fight for your life!")
+
+# Tavern IS PlaceBase?
+# PlaceBase HAS BattleOfClasse
--- BattleOfClasses/main.py
+++ BattleOfClasses/main.py
@@ -1,10 +1,34 @@
 import os
+from game_assets.characters import Player
+from game_assets.places import Arena, Tavern
 
 
 class BattleOfClasses:
     def __init__(self):
-        pass
+        self.clear_screen()
+        self.intro()
+
+        self._player = Player()
+
+        # places
+        self._tavern = Tavern(self)
+        self._arena = Arena(self)
+
+        # A is B or A has B
+
+        self._arena.enter()
+
+    @property
+    def player(self):
+        return self._player
+
+    def intro(self):
+        print("-"*50, "BATTLE OF CLASSES", "-"*50)
 
     @staticmethod
     def clear_screen():
+        print("clear_screen called")
         os.system("cls")
+
+
+BattleOfClasses()
